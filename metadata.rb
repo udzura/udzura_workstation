3ec9df27308bdc97bf51dd3f2a2198695a14663c
Uchio KONDO <udzura@paperboy.co.jp>
2013-10-25 13:03:56 +0900

    Add tmux conf, fix some descs

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             'udzura_workstation'
-maintainer       'YOUR_COMPANY_NAME'
-maintainer_email 'YOUR_EMAIL'
-license          'All rights reserved'
+maintainer       'Uchio KONDO'
+maintainer_email 'udzura@udzura.jp'
+license          'Apache License'
 description      'Installs/Configures udzura_workstation'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.20131025'
--- recipes/default.rb
+++ recipes/default.rb
@@ -2,7 +2,21 @@
 # Cookbook Name:: udzura_workstation
 # Recipe:: default
 #
-# Copyright 2013, YOUR_COMPANY_NAME
+# Copyright 2013, Uchio KONDO
 #
-# All rights reserved - Do Not Redistribute
+# All rights reserved - see README
 #
+
+# TODO: split 'em out in the future
+
+## Dotfilez
+
+file "#{node['sprout']['home']}/.tmux.conf" do
+  mode "0644"
+  owner node['current_user']
+  action :create, :update
+end
+
+## Pow
+
+brew "pow"

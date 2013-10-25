0b8aa71fd39c6181a1121eb03c9deab5f84be280
Uchio KONDO <udzura@udzura.jp>
2013-10-25 13:08:33 +0900

    Add supported platform

--- metadata.rb
+++ metadata.rb
@@ -2,6 +2,7 @@ name             'udzura_workstation'
 maintainer       'Uchio KONDO'
 maintainer_email 'udzura@udzura.jp'
 license          'Apache License'
-description      'Installs/Configures udzura_workstation'
+description      'The udzura_workstation'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.20131025'
+supports         'mac_os_x'

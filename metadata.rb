a23734295b89a75203e3daa44f7eef8006b0536d
Uchio KONDO <udzura@udzura.jp>
2014-01-07 22:12:42 +0900

    Version bump

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'udzura@udzura.jp'
 license          'Apache License'
 description      'The udzura_workstation'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.20131025'
+version          '0.2.20140107'
 supports         'mac_os_x'
 depends          'sprout-osx-base'
 depends          'sprout-osx-settings'
